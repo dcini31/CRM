fa476c5d998d859ede2f617967beb8db89c8646b
dcini31 <73096398+dcini31@users.noreply.github.com>
2023-10-20 23:29:22 +0200

    validation on employee create

--- crm/resources/views/employee/create.blade.php
+++ crm/resources/views/employee/create.blade.php
@@ -2,39 +2,58 @@
 @section('content')
     <h1>Create</h1>
     <div class="card-body">
-        {{-- // --}}
-        <div class="table-responsive">
-            <form method="post" action="{{ route('employee/store') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <label for="first_name">First Name</label>
-                    <input type="text" name="first_name" class="form-control" autocomplete="on" id="first_name"
-                        aria-describedby="" placeholder="First Name">
-                </div>
-                <div class="form-group">
-                    <label for="last_name">Last Name</label>
-                    <input type="text" name="last_name" class="form-control" autocomplete="on" id="last_name"
-                        placeholder="Surname">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" class="form-control" autocomplete="on" id="email"
-                        placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <label for="phone">Phone</label>
-                    <input type="numeric" name="phone" class="form-control" autocomplete="on" id="phone"
-                        placeholder="Phone Number">
-                </div>
-                <div class="form-group">
-                    <label for="company_id">Company</label>
-                    <select name="company_id" class="form-control" id="company_id">
-                        <option value="" disabled selected>Select a company</option>
-                        @foreach ($companies as $company)
-                            <option value="{{ $company->id }}">{{ $company->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
+        <form method="post" action="{{ route('employee/store') }}" enctype="multipart/form-data">
+            @csrf
 
-                <button type="submit" class="btn btn-primary">Create</button>
-            @endsection
+            <div class="form-group">
+                <label for="first_name">First Name</label>
+                <input type="text" name="first_name" class="form-control @error('first_name') is-invalid @enderror"
+                    autocomplete="on" id="first_name" aria-describedby="" placeholder="First Name">
+                @error('first_name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="last_name">Last Name</label>
+                <input type="text" name="last_name" class="form-control @error('last_name') is-invalid @enderror"
+                    autocomplete="on" id="last_name" placeholder="Surname">
+                @error('last_name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                    autocomplete="on" id="email" placeholder="Email">
+                @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="phone">Phone</label>
+                <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror"
+                    autocomplete="on" id="phone" placeholder="Phone Number">
+                @error('phone')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="company_id">Company</label>
+                <select name="company_id" class="form-control @error('company_id') is-invalid @enderror" id="company_id">
+                    <option value="" disabled selected>Select a company</option>
+                    @foreach ($companies as $company)
+                        <option value="{{ $company->id }}">{{ $company->name }}</option>
+                    @endforeach
+                </select>
+                @error('company_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    @endsection
